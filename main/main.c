53fb25e940fe2214495963f2d9200186d6b4c625
marques-svnt <marques.svnt.002@gmail.com>
2025-02-03 20:22:49 -0300

    exportação de códigos e arquivos de projetos anteriores:
    PIO - Configuração do ws2812.pio para acender os leds 5x5
    
    pinos - para inicializar os pinos
    
    interrupt - para configurar a rotina de interrupção e debounce

--- main/main.c
+++ main/main.c
@@ -1,11 +1,22 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
-
+#include <stdio.h>
+#include <stdlib.h>
+#include "pico/stdlib.h"
+#include "hardware/pio.h"
+#include "hardware/clocks.h"
+#include "pio.h"
+#include "pinos.h"
+#include "interrupt.h"
 
 
 int main()
 {
+    // Inicializações
+    pinosInit();
     stdio_init_all();
+    initializePio();
+
 
     while (true) {
         printf("Hello, world!\n");
