b217cf4d1d8731a086fe2d7051b37c90b2c52954
marques-svnt <marques.svnt.002@gmail.com>
2025-02-05 00:09:52 -0300

    Adicionado comuniicação UART e corrigido fixes de desempenho no wokwi. Próxima etapa: Configurar o display OLED, fonte minuscula e criar as correspondecias solicitadas pela tarefa

--- main.c
+++ main.c
@@ -8,6 +8,7 @@
 #include "pio.h"
 #include "pinos.h"
 #include "interrupt.h"
+#include "uart.h"
 
 
 int main()
@@ -22,6 +23,7 @@ int main()
     gpio_set_irq_enabled_with_callback(BUTTON_B, GPIO_IRQ_EDGE_FALL, true, &gpio_irq_handler);
 
     while (true) {
+        uart_handler();
         sleep_ms(1000);
     }
 }
---
+++ src/uart.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "pico/stdlib.h"
+#include "hardware/pio.h"
+#include "hardware/clocks.h"
+#include "pio.h"
+
+// Variável global para armazenar a cor (Entre 0 e 255 para intensidade)
+uint8_t led_r = 0;   // Intensidade do vermelho
+uint8_t led_g = 0;   // Intensidade do verde
+uint8_t led_b = 100; // Intensidade do azul
+
+void uart_handler()
+{
+    if (stdio_usb_connected())
+    { // Certifica-se de que o USB está conectado
+        char c;
+        printf("Digite um comando (0 a 9): \n");
+        if (scanf("%c", &c) == 1) // Lê caractere da entrada padrão
+        {
+            printf("Recebido: '%c'\n", c);
+
+            // Vai checar qual comando foi recebido e fazer a ligação do led apropriado
+            switch (c)
+            {
+            case '0':
+                set_one_led(0, led_r, led_g, led_b);
+                break;
+            case '1':
+                set_one_led(1, led_r, led_g, led_b);
+                break;
+            case '2':
+                set_one_led(2, led_r, led_g, led_b);
+                break;
+            case '3':
+                set_one_led(3, led_r, led_g, led_b);
+                break;
+            case '4':
+                set_one_led(4, led_r, led_g, led_b);
+                break;
+            case '5':
+                set_one_led(5, led_r, led_g, led_b);
+                break;
+            case '6':
+                set_one_led(6, led_r, led_g, led_b);
+                break;
+            case '7':
+                set_one_led(7, led_r, led_g, led_b);
+                break;
+            case '8':
+                set_one_led(8, led_r, led_g, led_b);
+                break;
+            case '9':
+                set_one_led(9, led_r, led_g, led_b);
+                break;
+            default:
+                printf("Comando inválido: '%c'\n", c);
+                break;
+            }
+        }
+    }
+}
